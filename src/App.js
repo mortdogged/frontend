0d39a7755f6974366beef31f0eaecff273bc10b5
Xiang Chen <xiangchenchen96@gmail.com>
2021-10-19 23:25:46 +0200

    :tada: First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import mortdog from "./radiant-mortdog.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={mortdog} className="App-logo" alt="logo" />
+        <p>Work in progress</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
