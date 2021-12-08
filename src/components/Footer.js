fe9d06d800edc0c77ea1d5f9799d31b02751957f
Cristiandgr <72732112+cristiandgra@users.noreply.github.com>
2021-12-08 02:12:31 +0100

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <section className="footer-section">
+      <div className="footer-container">
+        <p className="footer-text">
+          mortdogged.com isn't endorsed by Riot Games and doesn't reflect the
+          views or opinions of Riot Games or anyone officially involved in
+          producing or managing Riot Games properties.
+        </p>
+        <p className="footer-text">
+          Riot Games, and all associated properties are trademarks or registered
+          trademarks of Riot Games, Inc.
+        </p>
+      </div>
+    </section>
+  );
+};
